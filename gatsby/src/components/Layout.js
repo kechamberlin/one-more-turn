049c6377e0f2fd785e0580c821db5f0a3825655c
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-11-23 11:44:01 -0800

    Moved Footer import to below Nav for natural flow, and imported normalize.css.

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Footer from './Footer';
 import Nav from './Nav';
+import Footer from './Footer';
+import 'normalize.css';
 
 export default function Layout({ children }) {
   return (
