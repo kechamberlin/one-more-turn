32fec0a27479de741b21def05879f19aa2d38af7
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 15:54:37 -0800

    Replaced comment with env import and export of various metadata, plugins.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,1 +1,24 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: `One More Turn`,
+    // siteUrl: 'https://onemoreturn.project.com',
+    description: 'A Civilization 6 gameplay tracker',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // This is the name of the added plugin
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'ju3mdkg6',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
