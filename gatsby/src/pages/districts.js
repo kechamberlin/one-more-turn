ec915f3845fe7beee62363dfe56a2f6d1612aeef
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 15:59:12 -0800

    Imported graphql, Link, Img, styled-components, and Pagination components.

--- gatsby/src/pages/districts.js
+++ gatsby/src/pages/districts.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import Pagination from '../components/Pagination';
 
 export default function DistrictsPage() {
   return (
