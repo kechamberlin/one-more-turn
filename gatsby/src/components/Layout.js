96d387416fe5a035cca044397e99bef205ff3610
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 15:16:04 -0800

    Imported GlobalStyles component and moved normalize.css import to below React import.

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import 'normalize.css';
 import Nav from './Nav';
 import Footer from './Footer';
-import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
 
 export default function Layout({ children }) {
   return (
