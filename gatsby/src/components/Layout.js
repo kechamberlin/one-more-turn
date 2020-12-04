5754b51a6e6b2dd5c633095947c1ccce8704677c
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 15:25:55 -0800

    Imported stripes svg.

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -5,6 +5,7 @@ import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import stripes from '../assets/images/stripes.svg';
 
 export default function Layout({ children }) {
   return (
