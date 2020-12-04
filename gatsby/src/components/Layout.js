c25868d5eb2b596be1333b297619aff973fd6e48
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 15:18:03 -0800

    Imported Typography component.

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -3,6 +3,7 @@ import 'normalize.css';
 import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
