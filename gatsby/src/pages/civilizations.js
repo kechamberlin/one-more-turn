d4f750e2b3fc8ee06f428adf1886f57ad2917475
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 15:58:34 -0800

    Queried allSanityCivilization data into civ page.

--- gatsby/src/pages/civilizations.js
+++ gatsby/src/pages/civilizations.js
@@ -10,3 +10,48 @@ export default function CivilizationsPage() {
     </>
   );
 }
+
+// TODO: after victories, add leaders { id name }
+export const query = graphql`
+  query($victory: [String]) {
+    civilizations: allSanityCivilization(
+      filter: { victories: { elemMatch: { name: { in: $victory } } } }
+    ) {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        victories {
+          id
+          name
+        }
+        leaders {
+          id
+          leader
+          image {
+            asset {
+              fixed(height: 100, width: 100) {
+                ...GatsbySanityImageFixed
+              }
+              fluid(maxWidth: 100) {
+                ...GatsbySanityImageFluid
+              }
+            }
+          }
+        }
+        image {
+          asset {
+            fixed(width: 65, height: 65) {
+              ...GatsbySanityImageFixed
+            }
+            fluid(maxWidth: 100) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
