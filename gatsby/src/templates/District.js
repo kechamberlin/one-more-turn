81a2cbe2424aa5d811c7363bf739eddf87e8f1e6
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 16:07:15 -0800

    Created a District template for displaying a single district.

---
+++ gatsby/src/templates/District.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+// import styled from 'styled-components';
+
+export default function DistrictPage({ data: { district } }) {
+  console.log(district);
+  return (
+    <div className="center">
+      <h2>
+        <span className="mark">{district.name}</span>
+      </h2>
+      <Img fluid={district.image.asset.fluid} />
+      <p>{district.description}</p>
+      <h2>Effects</h2>
+      <ul>
+        {district.adjacency.map((test) => (
+          <li>{test}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    district: sanityDistrict(slug: { current: { eq: $slug } }) {
+      id
+      name
+      description
+      adjacency
+      image {
+        asset {
+          fluid(maxWidth: 1000, maxHeight: 750) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+  }
+`;
