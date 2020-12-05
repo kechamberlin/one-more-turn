a113fccd6be4d9af60f5ccb3f53a03e90e2ddfbe
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-04 16:06:51 -0800

    Created a Civilization template for displaying a single civ.

---
+++ gatsby/src/templates/Civilization.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+
+const CivilizationGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+export default function SingleCivPage({ data }) {
+  const { civilization } = data;
+  return (
+    <CivilizationGrid>
+      <Img fluid={civilization.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{civilization.name}</h2>
+        <ul>
+          {civilization.victories.map((victory) => (
+            <li key={victory.id}>{victory.name}</li>
+          ))}
+        </ul>
+      </div>
+    </CivilizationGrid>
+  );
+}
+
+// This needs to be dynamic based on the slug passed in via context in gatsby-node.
+export const query = graphql`
+  query($slug: String!) {
+    civilization: sanityCivilization(slug: { current: { eq: $slug } }) {
+      name
+      id
+      image {
+        asset {
+          fluid(maxWidth: 200) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      victories {
+        name
+        id
+      }
+    }
+  }
+`;
