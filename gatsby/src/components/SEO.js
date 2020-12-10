6a0a1e76b65e1d2d41d3928608efb6ef76d6d704
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-09 17:59:40 -0800

    Added meta tags to SEO file.

--- gatsby/src/components/SEO.js
+++ gatsby/src/components/SEO.js
@@ -17,7 +17,22 @@ export default function SEO({ children, location, description, title, image }) {
     <Helmet titleTemplate={`%s - ${site.siteMetadata.title}`}>
       <html lang="en" />
       <title>{title}</title>
+      {/* Fav Icon */}
       <link rel="icon" href="/favicon.ico" />
+      {/* Meta Tags */}
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta charSet="utf-8" />
+      <meta name="description" content={site.siteMetadata.description} />
+      {location && <meta property="og:url" content={location.href} />}
+      <meta property="og:image" content={image || '/favicon.ico'} />
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta
+        property="og:site_name"
+        content={site.siteMetadata.title}
+        key="ogsitename"
+      />
+      <meta property="og:description" content={description} key="ogdesc" />
+      {children}
     </Helmet>
   );
 }
