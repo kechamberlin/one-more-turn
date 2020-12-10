5b39b0409f75c665e074912637fcc88b67b0a895
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-09 17:54:23 -0800

    Removed stripes, including import.

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -5,13 +5,11 @@ import Nav from './Nav';
 import Footer from './Footer';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
-import stripes from '../assets/images/stripes.svg';
 
 const SiteBorderStyles = styled.div`
   max-width: 1000px;
   margin: 12rem auto 4rem auto;
   margin-top: clamp(2rem, 10vw, 12rem);
-  background: white url(${stripes});
   background-size: 1500px;
   padding: 5px;
   padding: clamp(5px, 1vw, 25px);
