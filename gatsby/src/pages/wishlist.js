4a9f31d3b5f6f1316e3306d036605a70dcfb3922
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-10 16:50:03 -0800

    Imported SEO component and added to return statement with title attribute.

--- gatsby/src/pages/wishlist.js
+++ gatsby/src/pages/wishlist.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import SEO from '../components/SEO';
 
 export default function WishListPage() {
   return (
     <>
+      <SEO title="Your Civ List" />
       <p>Wish List Page</p>
     </>
   );
