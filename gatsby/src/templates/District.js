c02feb3a4c8f144abb078d522a97ba0c1d860ffe
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-10 16:47:29 -0800

    Wrapped return statement in React Fragment.  Added SEO component above div tag.  Changed test parameter to bonus.

--- gatsby/src/templates/District.js
+++ gatsby/src/templates/District.js
@@ -7,19 +7,22 @@ import SEO from '../components/SEO';
 export default function DistrictPage({ data: { district } }) {
   console.log(district);
   return (
-    <div className="center">
-      <h2>
-        <span className="mark">{district.name}</span>
-      </h2>
-      <Img fluid={district.image.asset.fluid} />
-      <p>{district.description}</p>
-      <h2>Effects</h2>
-      <ul>
-        {district.adjacency.map((test) => (
-          <li>{test}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <SEO title={district.name} image={district.image?.asset?.fluid?.src} />
+      <div className="center">
+        <h2>
+          <span className="mark">{district.name}</span>
+        </h2>
+        <Img fluid={district.image.asset.fluid} />
+        <p>{district.description}</p>
+        <h2>Effects</h2>
+        <ul>
+          {district.adjacency.map((bonus) => (
+            <li>{bonus}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
