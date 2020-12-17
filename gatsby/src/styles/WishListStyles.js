a55fcb8618009a993431f0fe2b9809a2d50cd9ab
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2020-12-17 13:55:37 -0800

    Created WishListStyles component with code from reference project.

---
+++ gatsby/src/styles/WishListStyles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const WishListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+  fieldset {
+    grid-column: span 2;
+    max-height: 600px;
+    overflow: auto;
+    display: grid;
+    gap: 1rem;
+    align-content: start;
+    &.order,
+    &.menu {
+      grid-column: span 1;
+      /* Chrome is weird about Grid and fieldsets, so we add a fixed height to fix it :)  */
+      height: 600px;
+    }
+  }
+  @media (max-width: 900px) {
+    fieldset.menu,
+    fieldset.order {
+      grid-column: span 2;
+    }
+  }
+`;
+
+export default WishListStyles;
