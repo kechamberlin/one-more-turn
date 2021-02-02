3aa798a6221b328b026e44a51bff7026ee989d16
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-01 16:26:06 -0800

    Imported styled-components and created FooterStyle component.

--- gatsby/src/components/Footer.js
+++ gatsby/src/components/Footer.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterStyle = styled.div`
+  text-align: center;
+  margin-top: 5rem;
+`;
 
 export default function Footer() {
   return (
