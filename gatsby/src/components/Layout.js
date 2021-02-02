b9e06f3826174dd0d79fbc86ee0565fec42c6d44
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-01 16:31:33 -0800

    Removed padding properties.

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -12,8 +12,6 @@ const SiteBorderStyles = styled.div`
   margin: 12rem auto 4rem auto;
   margin-top: clamp(2rem, 10vw, 12rem);
   background-size: 1500px;
-  padding: 5px;
-  padding: clamp(5px, 1vw, 25px);
   box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
   border: 5px solid white;
   @media (max-width: 1100px) {
