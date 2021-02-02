b632368cc7336bd66fd6006d79cac2196336ddf2
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-02 09:24:23 -0800

    Imported styled-components, created SingleDistrictGrid component with styling.

--- gatsby/src/templates/District.js
+++ gatsby/src/templates/District.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import styled from 'styled-components';
 import SEO from '../components/SEO';
+
+const SingleDistrictGrid = styled.div`
+  display: grid;
+  grid-gap: 1rem;
+  grid-template-columns: 1fr 1fr;
+  text-align: center;
+  margin-top: 4rem;
+  li {
+    margin-top: 10px;
+    list-style-type: circle;
+  }
+  li:first-child {
+    margin-top: 0;
+  }
+  .district-name {
+    margin-bottom: 2rem;
+  }
+  @media (max-width: 768px) {
+    font-size: 1.5rem;
+  }
+  @media (max-width: 700px) {
+    display: block;
+    font-size: 1.35rem;
+    h1 {
+      font-size: 3rem;
+    }
+    .district-effect {
+      margin-top: 3rem;
+    }
+  }
+  @media (max-width: 425px) {
+    font-size: 1.25rem;
+  }
+`;
 
 export default function DistrictPage({ data: { district } }) {
   return (
