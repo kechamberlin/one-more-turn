1777dde33fdecfcc4ec374ad43d6fca18d91543d
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-01 16:55:34 -0800

    Replaced reference project font code with new font family, style, weight.

--- gatsby/src/styles/Typography.js
+++ gatsby/src/styles/Typography.js
@@ -1,14 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
-import font from '../assets/fonts/frenchfries.woff';
-
 const Typography = createGlobalStyle`
-  @font-face {
-    font-family: FrenchFries;
-    src: url(${font});
-  }
   html {
-    font-family: FrenchFries, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: brandon-grotesque, sans-serif;
+    font-style: regular;
+    font-weight: 100;    
     color: var(--black);
   }
   p, li {
