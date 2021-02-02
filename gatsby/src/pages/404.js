3651f518f8afd8c41e5841f68699992358ab4b4e
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-01 16:49:33 -0800

    Imported styled-components, created FourOhFourStyles component, replaced React fragment with styled-component, added h1, changed p tag text.

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FourOhFourStyles = styled.div`
+  text-align: center;
+`;
 
 export default function FourOhFourPage() {
   return (
-    <>
-      <p>This page does not exist</p>
-    </>
+    <FourOhFourStyles>
+      <h1>Error 404</h1>
+      <p>Looks like Barbarians pillaged this page.</p>
+    </FourOhFourStyles>
   );
 }
