c356aa251eccd2446f900f5c0f7ea0aaf7bb3407
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-01 16:35:21 -0800

    Removed leftover reference project Logo code.

--- gatsby/src/components/Logo.js
+++ gatsby/src/components/Logo.js
@@ -17,20 +17,6 @@ const LogoStyles = styled.div`
 export default function Logo() {
   return (
     <LogoStyles className="logo">
-      <div className="inner">
-        <span className="est">EST 1994</span>
-        <h1>
-          <span className="slicks">
-            <span className="letter S">S</span>
-            <span className="letter l">l</span>
-            <span className="letter i">i</span>
-            <span className="letter c">c</span>
-            <span className="letter k">k</span>
-            <span className="letter apos">'</span>
-            <span className="letter s">s</span>
-          </span>
-          <span className="slices">slices</span>
-        </h1>
       </div>
     </LogoStyles>
   );
