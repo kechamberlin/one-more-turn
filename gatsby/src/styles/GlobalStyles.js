e97057ff6911208a73ffa4519639d8f80a5ff00e
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-02 09:11:08 -0800

    Changed bg import to image that looks like game home screen.

--- gatsby/src/styles/GlobalStyles.js
+++ gatsby/src/styles/GlobalStyles.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
-import bg from '../assets/images/bg_stone_repeat.webp';
+import bg from '../assets/images/civ-menu.jpg';
 
 const GlobalStyles = createGlobalStyle`
   :root {
