d29fd7a53f688cfb0d47d3cc648a7897401db5d3
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-02-01 16:39:03 -0800

    Removed unneeded code from reference project.

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -17,24 +17,6 @@ const NavStyles = styled.nav`
     grid-template-columns: auto 1fr 1fr 1fr 1fr;
     grid-gap: 2rem;
     align-items: center;
-    margin-top: -6rem;
-  }
-  li {
-    --rotate: -2deg;
-    transform: rotate(var(--rotate));
-    order: 1;
-    &:nth-child(1) {
-      --rotate: 1deg;
-    }
-    &:nth-child(2) {
-      --rotate: -2.5deg;
-    }
-    &:nth-child(4) {
-      --rotate: 2.5deg;
-    }
-    &:hover {
-      --rotate: 3deg;
-    }
   }
   a {
     font-size: 3rem;
